64b9b102c821d36f1f3c29f03ffda4b99d2651c5
shubhadeepskr <97300239+shubhadeepskr@users.noreply.github.com>
2023-01-12 15:38:56 +0530

    Update streamlit_app.py
    Change title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('I will make hard coffee this evening')
+streamlit.title('Cafe Menu')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
