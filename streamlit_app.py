876eae9cc56600a303779e2b8952774309210164
shubhadeepskr <97300239+shubhadeepskr@users.noreply.github.com>
2023-01-12 15:35:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('I will make hard coffee this evening')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
