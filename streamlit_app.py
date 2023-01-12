d56b6f396b55bd393b6ab3a6c46802e0e87c1f25
shubhadeepskr <97300239+shubhadeepskr@users.noreply.github.com>
2023-01-12 15:51:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,6 @@ streamlit.text('>>Hard-Boiled Free-Range Egg')
 
 streamlit.header('Build your own smoothie!')
 
-my_fruit_list=pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.dataframe(my_fruit_list)
